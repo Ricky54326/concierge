3dcfb8684e5992cf4329a09b372c84ec00d74ef1
Alex Chen <axcqwerty@gmail.com>
2013-07-28 03:04:35 -0700

    Add parse util. might not be used.

---
+++ Concierge/ParseUtil.h
@@ -0,0 +1,15 @@
+//
+//  ParseUtil.h
+//  Concierge
+//
+//  Created by Alex Chen on 7/28/13.
+//  Copyright (c) 2013 Katie Siegel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParseUtil : NSObject
+
+- (NSMutableArray *)getSponsors;
+
+@end
