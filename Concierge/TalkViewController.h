bbb357ef4048b2529153755363c9da92b240e257
Cathie <cathieyun@gmail.com>
2013-07-28 04:39:17 -0700

    get the chat stuff workingzzz

--- Concierge/TalkViewController.h
+++ Concierge/TalkViewController.h
@@ -8,6 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TalkViewController : UITableViewController
+@interface TalkViewController : UITableViewController <UITableViewDataSource,UITableViewDelegate>	{
+	IBOutlet UITextField *messageText;
+	IBOutlet UIButton *sendButton;
+	IBOutlet UITableView *messageList;
+	int lastId;
+	
+	NSMutableData *receivedData;
+	
+	NSMutableArray *messages;
+	
+	NSTimer *timer;
+	
+	NSXMLParser *chatParser;
+	NSString *msgAdded;
+	NSMutableString *msgUser;
+	NSMutableString *msgText;
+	int msgId;
+	Boolean inText;
+	Boolean inUser;
+}
+
+@property (nonatomic,retain) UITextField *messageText;
+@property (nonatomic,retain) UIButton *sendButton;
+@property (nonatomic,retain) UITableView *messageList;
+
+- (IBAction)sendClicked:(id)sender;
 
 @end
